1a9f39c839564bf29583784dc049a4b3d3b2623e
Hsuyining <m0804106@im.tpcu.edu.tw>
2019-10-03 20:40:00 +0800

    Signed-off-by: Hsuyining <m0804106@im.tpcu.edu.tw> 1024

---
+++ 1024R.R
@@ -0,0 +1,44 @@
+X=100:110
+X
+
+X<-1:6
+X+3
+X + 6:7
+ 
+seq(1,9)
+seq(1.5,4.5,by=0.5)
+seq(1,9,length.out = 5)
+x<-c(1,3,7,2,9)
+X
+basket.NBA2016.Lin<-c(7,8,6,11,9,12)
+basket.NBA2016.Lin
+scores.NBA2016.Lin<-basket.NBA2016.Lin*2
+scores.NBA2016.Lin
+baskets.NBA2016.Lin<-c(7,8,6,11,9,12)
+baskets.NBA2016.Jordon<-c(10,5,9,12,7,11)
+baskets.NBA2016.Jordon+baskets.NBA2016.Lin
+(baskets.NBA2016.Jordon+baskets.NBA2016.Lin)*2
+rep(5,5)
+rep(5,times=5)
+rep(1:5,3)
+rep(1:3,times=3,each=2)
+rep(1:3,each=2,length.out=8)
+x<-numeric(10)
+x
+all.baskets.NBA2016<-basket.NBA2016.Jordon +
+  baskets.NBA2016.Lin
+basket.NBA2016.Lin<-c(7,8,6,11,9,12)
+sum(baskets.NBA2016.Lin)
+max(baskets.NBA2016.Lin)
+min(baskets.NBA2016.Lin)
+mean(baskets.NBA2016.Lin)
+cumsum(baskets.NBA2016.Jordon)
+cumproud(baskets.NBA2016.Jordon)
+x=sample(1:100,10)
+X
+sort(X)
+rank(X)
+rank(-X)
+length(x)
+x=(hello)
+length(X)
---
+++ ex1.R
@@ -0,0 +1,6 @@
+a=3
+class(a)
+b="taipei"
+class(b)
+c=3+2i
+class(C)
